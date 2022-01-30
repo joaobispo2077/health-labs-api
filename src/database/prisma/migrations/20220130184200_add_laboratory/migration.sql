b34f2758a84603fa2f2b71f967110cb8be988a14
João Bispo <joaobispo2077@gmail.com>
2022-01-30 15:43:49 -0300

    feat: setup Prisma ORM

---
+++ src/database/prisma/migrations/20220130184200_add_laboratory/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "laboratory" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+
+    CONSTRAINT "laboratory_pkey" PRIMARY KEY ("id")
+);
--- src/repositories/LaboratoriesRepositories.ts
+++ src/repositories/LaboratoriesRepositories.ts
@@ -1,5 +1,5 @@
+import { CreateLaboratoryDTO } from '@src/dtos/LaboratoriesDTOS';
 import { Laboratory } from '@src/entities/Laboratory';
-import { CreateLaboratoryDTO } from '@src/services/LaboratoriesServices/dtos';
 
 export interface LaboratoriesRepositories {
   create({ name, address, status }: CreateLaboratoryDTO): Promise<Laboratory>;
